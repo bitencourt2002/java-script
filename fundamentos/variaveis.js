35944da0a1ba8f3c8d4ea2864779d13a4d6d5bfd
gustavo santana <gustavosantana562@gmail.com>
2026-04-08 22:23:50 -0300

    variaveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,46 @@
+/**
+ * estudo das variaveis
+ * @autor gustavo santana
+ */
+console.clear()
+console.log('estudo das variaveis')
+console.log('')
+console.log('variavel do tipo string')
+// a linha abaixo cria uma variavel
+let nome   = 'gustavo santana'
+console.log(nome)
+// a linha abaixo mostra o tipo de variavel
+console.log(typeof (nome))
+console.log('')
+console.log('variavel do tipo number')
+let idade = 24
+console.log(idade)
+console.log(typeof(idade))
+let peso = 65
+console.log(peso)
+console.log(typeof(peso))
+
+let altura = 1.82
+console.log(altura)
+console.log(typeof(altura))
+console.log('')
+console.log('variavel do tipo boolean')
+let vip = true
+console.log(vip)
+console.log(typeof(vip))
+
+console.log('')
+console.log('------------------------------')
+console.log(' ficha do aluno ')
+console.log('------------------------------')
+// a linha abaixo concatena (uniao) um texto com o 
+//conteudo 
+console.log(`nome: ${nome}`)
+console.log(`idade: ${idade}`)
+
+
+
+console.log(`peso: ${peso}`)
+console.log(`altura: ${altura}`)
+console.log(`vip: ${vip}`)
+
